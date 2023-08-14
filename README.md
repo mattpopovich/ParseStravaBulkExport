b689ab76c6c2ee62d7193e65ee864e95e5225eb8
Matt Popovich <popovich.matt@gmail.com>
2023-08-14 00:44:05 -0600

    Exported folder now only contains unzipped files

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # ParseStravaBulkExport
-Strava will let you export all your activities, but it is up to you to find out which activities correspond to biking vs. running, etc.
+
+#### The Problem:
+Strava will let you export all your activities just fine, but it is up to you to find out which activities correspond to biking vs. running, etc. Thankfully, they give you a nice `activities.csv` file telling you some details for each file in the `activities` folder.
+
+#### The Solution:
+Parse that `activities.csv` file to pull out the activities we want based on certain parameters.
+
+## Use
+Enter the Dockerfile, then
+```console
+$ python main.py
+```
--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import sys                  # sys.exit()
 import glob
 import argparse
 import shutil
+import gzip
+import fnmatch
 
 from typing import Final, List    # Typing hints: Final[variable type]
 
@@ -18,17 +20,25 @@ parser.add_argument("--overwrite", action="store_true", help="Overwrite previous
 args = parser.parse_args()
 
 # Define the path to your CSV file
-EXPORTED_FOLDER_PATH: Final[str] = config['DEFAULT']['EXPORTED_FOLDER_PATH']
-ACTIVITIES_FOLDER_PATH: Final[str] = EXPORTED_FOLDER_PATH + "/activities"
-csv_file_path: Final[str] = EXPORTED_FOLDER_PATH + "/activities.csv"
+STRAVA_EXPORTED_FOLDER_PATH: Final[str] = config['DEFAULT']['STRAVA_EXPORTED_FOLDER_PATH']
+ACTIVITIES_FOLDER_PATH: Final[str] = STRAVA_EXPORTED_FOLDER_PATH + "/activities"
+csv_file_path: Final[str] = STRAVA_EXPORTED_FOLDER_PATH + "/activities.csv"
+
+# Mark the exported folder path as read only so that we don't accidentally modify anything
+ORIGINAL_FOLDER_PERMISSIONS = os.stat(STRAVA_EXPORTED_FOLDER_PATH).st_mode & 0o777
+# Folders are 755
+# Files are 644
+os.chmod(STRAVA_EXPORTED_FOLDER_PATH, 0o555)
+os.chmod(ACTIVITIES_FOLDER_PATH, 0o555)
+os.chmod(csv_file_path, 0o444)
 
 # Check if the CSV file exists before reading
 if csv_file_path == "strava_export_########":
-    print("Please modify the `EXPORTED_FOLDER_PATH` variable in config.cfg to "
+    print("Please modify the `STRAVA_EXPORTED_FOLDER_PATH` variable in config.cfg to "
           "match your exported folder name")
 elif not os.path.exists(csv_file_path):
     print(f"The CSV file '{csv_file_path}' does not exist, please update "
-           "`EXPORTED_FOLDER_PATH` in config.cfg")
+           "`STRAVA_EXPORTED_FOLDER_PATH` in config.cfg")
 
 # Define the Activity Type you want to filter for
 target_activity_type = 'Ride'
@@ -36,27 +46,27 @@ target_activity_type = 'Ride'
 # Initialize a list to store Activity IDs
 ride_activity_filenames: List[str] = []
 
-# Read the CSV file and extract Activity IDs for the specified Activity Type
+# Read the CSV file and extract the filenames for the specified Activity Type
 with open(csv_file_path, 'r', newline='', encoding='utf-8') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         if row['Activity Type'] == target_activity_type:
-            ride_activity_filenames.append(EXPORTED_FOLDER_PATH + "/" + row['Filename'])
+            ride_activity_filenames.append(STRAVA_EXPORTED_FOLDER_PATH + "/" + row['Filename'])
 
-# Print the list of Activity IDs for "Ride" activities
-print("Activity IDs for 'Ride' activities:")
-for activity_id in ride_activity_filenames:
-    print(activity_id)
+# Print the list of filenames for "Ride" activities
+print("Filenames for 'Ride' activities:")
+for filename in ride_activity_filenames:
+    print("\t" + filename)
 
 # Create rides folder
 EXPORT_FOLDER_NAME: Final[str] = "exports"
 RIDES_FOLDER_NAME: str = EXPORT_FOLDER_NAME + "/rides"
 if os.path.exists(RIDES_FOLDER_NAME):
     if args.overwrite:
-        os.rmdir(RIDES_FOLDER_NAME)
+        shutil.rmtree(RIDES_FOLDER_NAME)
     else:
         sys.exit(f"{RIDES_FOLDER_NAME} folder already exists,"
-            " please remove it then run this script again")
+                  " please remove it then run this script again")
 
 os.makedirs(RIDES_FOLDER_NAME)
 
@@ -65,3 +75,22 @@ for filename in ride_activity_filenames:
     shutil.copy(filename, RIDES_FOLDER_NAME + "/")
 
 print(f"Copied {len(ride_activity_filenames)} files to the {RIDES_FOLDER_NAME} folder")
+
+# Unzip any zipped up folders
+zipped_up_file_paths: List[str] = fnmatch.filter(ride_activity_filenames, "*.gz")
+if len(zipped_up_file_paths) > 0:
+    for zipped_up_file_path in zipped_up_file_paths:
+        zipped_up_filename = os.path.basename(zipped_up_file_path)
+        with gzip.open(zipped_up_file_path, 'rb') as f_zip:
+            unzipped_filename: str = zipped_up_filename.rstrip(".gz")
+            with open(RIDES_FOLDER_NAME + "/" + unzipped_filename, 'wb') as f_unzip:
+                shutil.copyfileobj(f_zip, f_unzip)
+                print(f"Unzipped {f_zip.name} to {f_unzip.name}")
+                os.remove(RIDES_FOLDER_NAME + "/" + zipped_up_filename)
+                print(f"Deleted {RIDES_FOLDER_NAME + '/' + zipped_up_filename}")
+
+# TODO: Replace this with an integration test that makes sure nothing was changed
+# Mark Strava's exported folder permissions back to what they were
+os.chmod(STRAVA_EXPORTED_FOLDER_PATH, ORIGINAL_FOLDER_PERMISSIONS)
+os.chmod(ACTIVITIES_FOLDER_PATH, ORIGINAL_FOLDER_PERMISSIONS)
+os.chmod(csv_file_path, 0o644)
